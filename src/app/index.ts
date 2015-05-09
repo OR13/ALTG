a0fc6d7f7a43808a196fb07c385a76b4824aae5d
Orie Steele <orie@ohs.io>
2015-05-09 14:35:18 -0500

    first commit

---
+++ src/app/index.ts
@@ -0,0 +1,27 @@
+/// <reference path="../../.tmp/typings/tsd.d.ts" />
+
+/// <reference path="main/main.controller.ts" />
+/// <reference path="../app/components/navbar/navbar.controller.ts" />
+
+module altg {
+  'use strict';
+
+  angular.module('altg', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
+    .controller('MainCtrl', MainCtrl)
+    .controller('NavbarCtrl', NavbarCtrl)
+
+  .config(function ($locationProvider: ng.ILocationProvider, $stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) {
+    
+    $locationProvider.html5Mode(true);
+    
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
+}
